9aa946eb9507afd5f2a662312ccb5ebc5af25e6d
cibere <71997063+cibere@users.noreply.github.com>
2025-02-27 11:47:54 -0800

    add Entry.copy

--- rtfm_lookup/entry.py
+++ rtfm_lookup/entry.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Any
+from typing import Any, Self
 
 import msgspec
 
@@ -11,3 +11,6 @@ class Entry(msgspec.Struct):
     text: str
     url: str
     options: dict[str, Any] = msgspec.field(default_factory=dict)
+
+    def copy(self) -> Self:
+        return Entry(text=self.text, url=self.url, options=self.options)  # pyright: ignore[reportReturnType]
